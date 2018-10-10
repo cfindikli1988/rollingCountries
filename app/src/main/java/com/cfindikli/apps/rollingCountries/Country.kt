11d6ba693eb5bbb2d4d3b4cdd6fd067809a5da26
rasat <cfindikli@outlook.com>
2018-10-10 13:44:54 +0300

    delete unnecessary blank lines.

--- app/src/main/java/com/cfindikli/apps/rollingCountries/Country.kt
+++ app/src/main/java/com/cfindikli/apps/rollingCountries/Country.kt
@@ -3,7 +3,6 @@ package com.cfindikli.apps.rollingCountries
 import android.net.Uri
 
 data class Country(var countryName: String? = null, var imageUrl: Uri? = null, var shortCode: String? = null, var flag: Int? = null) {
-
     var level: Int = 0
     var levelName = listOf("Round 1", "Round 2", "Round 3", "Round 4", "Round 5", "QuarterFinal", "SemiFinal", "Final")
     var sum: Int = 0
